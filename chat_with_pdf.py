2538136ce85a523d19c59aa141d14b18cd5e9945
cursedseraphim <christian.steinparz@gmail.com>
2023-09-19 09:59:10 +0200

    query llm and pdf when llm text is entered

--- chat_with_pdf.py
+++ chat_with_pdf.py
@@ -2,21 +2,18 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
-
 from langchain import FAISS
 from langchain.callbacks import get_openai_callback
 from langchain.chains.question_answering import load_qa_chain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
+from PyPDF2 import PdfReader
 
 # Load environment variables
 load_dotenv()
 
-
 def process_text(text):
-    # Split the text into chunks using Langchain's CharacterTextSplitter
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
@@ -24,31 +21,26 @@ def process_text(text):
         length_function=len
     )
     chunks = text_splitter.split_text(text)
-    
-    # Convert the chunks of text into embeddings to form a knowledge base
     embeddings = OpenAIEmbeddings()
     knowledgeBase = FAISS.from_texts(chunks, embeddings)
-    
     return knowledgeBase
-
 
 def main():
     st.title("Chat with your PDF 💬")
     
-    # Dropdown for model selection
     model_name = st.selectbox("Select the LLM Model:", ('gpt-4', 'gpt-3.5-turbo'))
     
-    query = st.text_input('Ask a question to the PDF')
+    search_query = st.text_input('Enter a query to search the PDF')
+    llm_query = st.text_input('Ask a question to the LLM')
+    
     pdf = st.file_uploader('Upload your PDF Document', type='pdf')
     
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
-        # Text variable will store the pdf text
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
         
-        # Create the knowledge base object
         knowledgeBase = process_text(text)
         
         cancel_button = st.button('Cancel')
@@ -56,19 +48,20 @@ def main():
         if cancel_button:
             st.stop()
         
-        if query:
-            docs = knowledgeBase.similarity_search(query)
-  
-        llm = OpenAI(model_name=model_name)  # Using the selected model_name
-        chain = load_qa_chain(llm, chain_type='stuff')
+        docs = None  # Initialize docs to None to use it later
+        if search_query or llm_query:  # Trigger PDF search if either query exists
+            docs = knowledgeBase.similarity_search(search_query if search_query else llm_query)
         
-        with get_openai_callback() as cost:
-            response = chain.run(input_documents=docs, question=query)
-            print(cost)
+        if llm_query:  # Trigger LLM search only if llm_query exists
+            llm = OpenAI(model_name=model_name)
+            chain = load_qa_chain(llm, chain_type='stuff')
             
-        st.write(response)
-        st.write(docs)
-            
-            
+            with get_openai_callback() as cost:
+                response = chain.run(input_documents=docs, question=llm_query)
+                print(cost)
+                
+            st.write(response)
+            st.write(docs)
+
 if __name__ == "__main__":
     main()
